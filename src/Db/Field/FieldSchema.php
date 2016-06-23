1fe7891816aac5bff89014b41d0593fb8aeaa0d6
Rdusy Natanael Acosta Crousset <natanael926@gmail.com>
2016-06-23 16:40:48 -0400

    Add methods setField, getField, getType

--- src/Db/Field/FieldSchema.php
+++ src/Db/Field/FieldSchema.php
@@ -1,8 +1,47 @@
 <?php
 
-namespace Ncrousset\GenCRUD\Db;
+namespace Ncrousset\GenCRUD\Db\Field;
 
-class FieldSchema
+
+class FieldSchema extends Field
 {
-    
+    /**
+     * @var
+     */
+    private $schema;
+
+    /**
+     * FieldSchema constructor.
+     * @param $schema
+     */
+    public function __construct($schema)
+    {
+        $this->schema = $schema;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->schema->type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getField()
+    {
+        return $this->schema->field;
+    }
+
+    /**
+     * 
+     * @param $field
+     * @return void
+     */
+    public function setField($field)
+    {
+        $this->schema->field = $field;
+    }
 }
