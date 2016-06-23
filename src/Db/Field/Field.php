614417eae9f3227bdae22a5a7a22555e5dc6e511
Rdusy Natanael Acosta Crousset <natanael926@gmail.com>
2016-06-23 10:08:27 -0400

    Created the main class for field

--- src/Db/Field/Field.php
+++ src/Db/Field/Field.php
@@ -1,15 +1,64 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rudys
- * Date: 23/06/16
- * Time: 10:00 AM
- */
 
 namespace Ncrousset\GenCRUD\Db\Field;
 
+use Ncrousset\GenCRUD\Db\Field\FieldSchemaInterface;
 
-class Field
+class Field implements FieldSchemaInterface
 {
 
+    /**
+     * @var string
+     */
+    public $field = "";
+
+    /**
+     * @var bool
+     */
+    public $nullable = false;
+
+    /**
+     * @var bool
+     */
+    public $primary = false;
+
+    /**
+     * @var bool
+     */
+    public $hidden = true;
+
+    /**
+     * Set the value name column
+     *
+     * @param $field
+     * @return void
+     */
+    public function field($field)
+    {
+        $this->field = $field;
+    }
+
+    /**
+     * @return void
+     */
+    public function nullable()
+    {
+        $this->nullable = true;
+    }
+
+    /**
+     * @return void
+     */
+    public function primary()
+    {
+        $this->primary = true;
+    }
+
+    /**
+     * @return void
+     */
+    public function hidden()
+    {
+        $this->hidden = true;
+    }
 }
