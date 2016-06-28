7a3a941f37fed5d5235084c069b5ac36fea937da
Rdusy Natanael Acosta Crousset <natanael926@gmail.com>
2016-06-28 12:27:34 -0400

    Create command GenCrud

--- src/Console/GenCrudCommand.php
+++ src/Console/GenCrudCommand.php
@@ -8,8 +8,37 @@
 
 namespace Ncrousset\GenCRUD\Console;
 
+use Illuminate\Console\Command;
 
-class GenCrudCommand
+class GenCrudCommand extends Command
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = "gencrud";
 
+    /**
+     * @var string
+     */
+    protected $description = "Execute the GenCrud";
+
+    /**
+     * GenCrudCommand constructor.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        echo  "Handle";
+    }
 }
